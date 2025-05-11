4f3346ff05aa801e1b427b9cc0c445c7405bbd8b
Gurdarshan Thind <thatguddugirl20@gmail.com>
2025-05-11 16:46:30 +0530

    Class 10 May

---
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi, Java 10th May 2025");
+
+        // This is Gurdarshan, This code will not executed
+
+        /*
+         * Author : Gurdarshan Thind
+         *
+         *
+         * */
+    }
+}
---
+++ src/ex_01_Java_Basics/Lab006_Comments_Multi.java
@@ -0,0 +1,28 @@
+package ex_01_Java_Basics;
+/**
+ * Author : Gurdarshan Thind
+ * Course :  ATB12x
+ * Learning :  Java
+ *
+ **/
+
+public class Lab006_Comments_Multi {
+
+    /**
+     * Author : Gurdarshan Thind
+     * Course :  ATB12x
+     * Learning :  Java
+     *
+     **/
+
+    public static void main(String[] args) {
+        System.out.println("Multi Line comment");
+        /**
+         * Author : Gurdarshan Thind
+         * Course :  ATB12x
+         * Learning :  Java
+         *
+         **/
+    }
+
+}
