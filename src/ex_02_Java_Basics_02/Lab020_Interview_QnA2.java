b2758dab8925a47c3ffc166706af2cd256f5a6e4
Gurdarshan Thind <thatguddugirl20@gmail.com>
2025-05-14 19:03:52 +0530

    class 4 May

---
+++ src/ex_02_Java_Basics_02/Lab020_Interview_QnA2.java
@@ -0,0 +1,14 @@
+package ex_02_Java_Basics_02;
+
+public class Lab020_Interview_QnA2 {
+    public static void main(String[] args) {
+         //int enum = 10;
+        int Enum = 10;
+        int a_b = 89;
+    //    int Gurdarshan thind  = 89;
+        int Gurdarshan$thind = 90;
+        int Lab020_Interview_QnA2 = 123;
+
+
+    }
+}
