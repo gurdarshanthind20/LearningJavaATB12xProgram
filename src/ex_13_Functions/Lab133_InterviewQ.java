a6b34d6d542d8dd379fa2f189195be51fc53f460
Gurdarshan Thind <thatguddugirl20@gmail.com>
2025-05-28 21:50:59 +0530

    Class P1 24 May

--- src/ex_13_Functions/Lab133_InterviewQ.java
+++ src/ex_13_Functions/Lab133_InterviewQ.java
@@ -4,20 +4,20 @@ public class Lab133_InterviewQ {
     public static void main(String[] args) {
 
     }
-    public static void main(String args) {
+    //public static void main(String args) {
 
     }
-    public static int main(int args) {
-        return 10;
-    }
-    public static int main(float args) {
-        return 10;
+    //public static int main(int args) {
+      //   return 10;
+   // }
+    //public static int main(float args) {
+      //  return 10;
 
-    }
-    public static boolean main(boolean args) {
-        return true;
-    }
-}
+   // }
+    //public static boolean main(boolean args) {
+    //    return true;
+    //}
+//}
 
 //This method is required by the Java Virtual Machine (JVM)
 // to start your program. In your code, this method exists,
